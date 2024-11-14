df1e7be367ad3a7b5432fe38fe87772c9ded2612
Zaina Khan <143953695+ZainaK05@users.noreply.github.com>
2024-11-14 11:00:13 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,44 +1,53 @@
 import os
 import socket
+import threading
 
-
-# IP = "192.168.1.101" #"localhost"
 IP = "localhost"
 PORT = 4450
 ADDR = (IP,PORT)
-SIZE = 1024 ## byte .. buffer size
+SIZE = 1024
 FORMAT = "utf-8"
-SERVER_DATA_PATH = "server_data"
+SERVER_PATH = "server"
+
+### to handle the clients
+def handle_client (conn,addr):
+
+
+    print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send("OK@Welcome to the server".encode(FORMAT))
+
+    while True:
+        data =  conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+       
+        send_data = "OK@"
+
+        if cmd == "LOGOUT":
+            break
+
+        elif cmd == "TASK": 
+            send_data += "LOGOUT from the server.\n"
+
+            conn.send(send_data.encode(FORMAT))
+
+
+
+    print(f"{addr} disconnected")
+    conn.close()
+
 
 def main():
-    
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(ADDR)
-    while True:  ### multiple communications
-        data = client.recv(SIZE).decode(FORMAT)
-        cmd, msg = data.split("@")
-        if cmd == "OK":
-            print(f"{msg}")
-        elif cmd == "DISCONNECTED":
-            print(f"{msg}")
-            break
-        
-        data = input("> ") 
-        data = data.split(" ")
-        cmd = data[0]
+    print("Starting the server")
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) ## used IPV4 and TCP connection
+    server.bind(ADDR) # bind the address
+    server.listen() ## start listening
+    print(f"server is listening on {IP}: {PORT}")
+    while True:
+        conn, addr = server.accept() ### accept a connection from a client
+        thread = threading.Thread(target = handle_client, args = (conn, addr)) ## assigning a thread for each client
+        thread.start()
 
-        if cmd == "TASK":
-            client.send(cmd.encode(FORMAT))
-
-        elif cmd == "LOGOUT":
-            client.send(cmd.encode(FORMAT))
-            break
-      
-
-
-
-    print("Disconnected from the server.")
-    client.close() ## close the connection
 
 if __name__ == "__main__":
     main()
