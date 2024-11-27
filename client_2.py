10fa95008ad13f073945f2e3efdc1b2ff2f127f1
TJ-Inman <106679597+TJ-Inman@users.noreply.github.com>
2024-11-27 14:12:48 -0500

    Update client_2.py
    use this to test file upload for the data analysis

--- client_2.py
+++ client_2.py
@@ -1,18 +1,46 @@
+
 import os
 import socket
+import threading
+import sys
+
+import tqdm
 
 
-IP = "localhost"
+IP = "127.0.0.1"
 PORT = 4450
 ADDR = (IP,PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 SERVER_DATA_PATH = "server_data"
 
+#filestoring protocol
+
+
+#filesharing protocol
+SEPARATOR = "<SEPARATOR>"
+#place a filename from the directory here:
+filename = "25MB.bin"
+filesize = os.path.getsize(filename)
+
 def main():
     
     client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client.connect(ADDR)
+
+    client.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            bytes_read = f.read(SIZE)
+            if not bytes_read:
+                break
+            client.sendall(bytes_read)
+            progress.update(len(bytes_read))
+
+
+
+
     while True:
         data = client.recv(SIZE).decode(FORMAT)
         cmd, msg = data.split("@")
@@ -38,3 +66,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
